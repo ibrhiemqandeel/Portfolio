93d7e29f0fab174b4510950a9160b74c7f604ea1
Ibrhime Qandeel <ibrahimgandeel@gmail.com>
2026-06-29 17:43:32 +0300

    Fix: Add debug log route

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,9 +1,13 @@
 <?php
 
+
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 
 class AppServiceProvider extends ServiceProvider
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/debug-log', function () {
+    $logPath = storage_path('logs/laravel.log');
+    if (file_exists($logPath)) {
+        return response(file_get_contents($logPath), 200)
+            ->header('Content-Type', 'text/plain');
+    }
+    return 'Log file does not exist yet.';
+});
